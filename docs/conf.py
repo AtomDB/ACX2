a51bb56884b0405d6dcd78a4fd7f0d18e211c60c
Adam Foster <afoster@cfa.harvard.edu>
2025-08-15 16:50:38 -0400

    documentation test

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ release = get_version('../__init__.py')
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -210,4 +210,4 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+#intersphinx_mapping = {'https://docs.python.org/': None}
